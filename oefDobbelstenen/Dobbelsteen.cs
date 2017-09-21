2aab2d68fdcc0897f4be223f30b9d996be5b7ed8
c1042421 <c1042421@hik.be>
2017-09-21 10:02:34 +0200

    added Dobbelsteen class

---
+++ oefDobbelstenen/Dobbelsteen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oefDobbelstenen
+{
+    class Dobbelsteen
+    {
+        private int _aantalZijden;
+        private int _waarde;
+        private Random _willGetal;
+
+        public int AantalZijden { get => _aantalZijden; set => _aantalZijden = value; }
+        public Random WillGetal { get => _willGetal; set => _willGetal = value; }
+        public int Waarde { get => _waarde; set => _waarde = value; }
+
+        public Dobbelsteen(int aantalZijden, Random r)
+        {
+            this.AantalZijden = aantalZijden;
+            this.WillGetal = r;
+        }
+
+        public Dobbelsteen(Random r) : this (6, r) { }
+
+        public void Roll()
+        {
+            this.Waarde = WillGetal.Next(1, AantalZijden);
+        }
+    }
+}
